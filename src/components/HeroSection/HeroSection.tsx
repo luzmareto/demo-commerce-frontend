d8c65e758788a94f6fb99b63752ca092043ae9f0
Luzmareto Rusmadian <900179@hq.bni.co.id>
2025-11-20 10:05:27 +0700

    done

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,33 +1,40 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 interface HeroSectionProps {
-    title: string;
-    subtitle: string;
+  title: string;
+  subtitle: string;
 }
 
 function HeroSection(props: HeroSectionProps) {
-    return (
-        <div className="hero">
-            <div className="container">
-                <div className="row justify-content-between">
-                    <div className="col-lg-5">
-                        <div className="intro-excerpt">
-                            <h1>{props.title}</h1>
-                            <p className="mb-4">{props.subtitle}</p>
-                            <p>
-                                <Link to="/shop" className="btn btn-secondary me-2">Belanja Sekarang</Link>
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col-lg-7">
-                        <div className="hero-img-wrap">
-                            <img src="/images/couch.png" className="img-fluid" alt="Couch" />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="hero-section">
+      <div className="container">
+        <div className="row align-items-center justify-content-between">
+
+          {/* TEXT AREA */}
+          <div className="col-12 col-md-6 col-lg-5">
+            <div className="intro-excerpt">
+              <h1>{props.title}</h1>
+              <p className="mb-4">{props.subtitle}</p>
+              <p>
+                <Link to="/shop" className="btn btn-secondary me-2">
+                  Belanja Sekarang
+                </Link>
+              </p>
             </div>
+          </div>
+
+          {/* IMAGE AREA */}
+          <div className="col-12 col-md-6 col-lg-7">
+            <div className="hero-img-wrap">
+              <img src="/images/couch.png" className="img-fluid hero-img" alt="Couch" />
+            </div>
+          </div>
+
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default HeroSection;
